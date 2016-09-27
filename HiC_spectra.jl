41703f013a0c4ae8768556a6fe715130e12e847a
Koon-Kiu Yan of group gerstein <ky26@grace0.grace.hpc.yale.internal>
2016-09-27 13:32:41 -0400

    add Knight Ruiz

--- HiC_spectra.jl
+++ HiC_spectra.jl
@@ -6,7 +6,6 @@ using MAT;
 #using CurveFit;
 #using Distributions;
 
-
 function get_Laplacian(M);
 	K=vec(sum(M,1));
 	i_nz=find(K.>0);
@@ -117,9 +116,80 @@ function evec_similarity(x,y)
 	#this is verified by simulation up to certain accuracy..not proved yet
 	s=abs(max_d-d)/max_d;
 
+	return s;
+
 end
-#it's very easy to transform evec_distance to evec_similarity,
-#since early reprducib
+#it's very easy to transform evec_distance to evec_similarity
+
+function knight_ruiz(A);
+#adopted from the MATLAB code implemented in Knight and Ruiz, 
+	n=size(A,1); 
+	e = ones(n,1); 
+	res=[];
+	delta = 0.1;
+	x0 = e;
+	tol = 1e-6;
+	g=0.9; etamax = 0.1; # Parameters used in inner stopping criterion.
+	
+	eta = etamax;
+	x = x0; rt = tol^2; v = x.*(A*x); rk = 1 - v;
+	rho_km1=sum(rk.^2);
+	rout = rho_km1; rold = rout;
+	MVP = 0; # count matrix vector products.
+	i = 0; # Outer iteration count.
+
+	while rout > rt # Outer iteration
+    	i = i + 1; k = 0; y = e;
+    	innertol = maximum([eta^2*rout;rt]);
+    	while rho_km1 > innertol #Inner iteration by CG
+        	k = k + 1;
+        	if k == 1
+            	Z = rk./v; p=Z; rho_km1 = sum(rk.*Z);
+        	else
+            	beta=rho_km1/rho_km2;
+            	p=Z + beta*p;
+        	end
+        	# Update search direction efficiently.
+        	w = x.*(A*(x.*p)) + v.*p;
+        	#w=squeeze(w,2);
+        	alpha = rho_km1/sum(p.*w);
+        	ap =squeeze(alpha*p,2);
+        	# Test distance to boundary of cone.
+        	ynew = y + ap;
+        	if minimum(ynew) <= delta
+            	if delta == 0
+            		break
+            	end
+            	ind = find(ap .< 0);
+            	gamma = minimum((delta - y[ind])./ap[ind]);
+            	y = y + gamma*ap;
+            	break
+        	end
+        	y = ynew;
+        	rk = rk - alpha*w; rho_km2 = rho_km1; rho_km2=rho_km2[1];
+        	Z = rk./v; rho_km1 = sum(rk.*Z);
+    	end
+    	x = x.*y; v = x.*(A*x);
+    	rk = 1 - v; rho_km1 = sum(rk.*rk); rout = rho_km1;
+    	MVP = MVP + k + 1;
+    	# Update inner iteration stopping criterion.
+    	rat = rout/rold; rold = rout; r_norm = sqrt(rout);
+    	eta_o = eta; eta = g*rat;
+    	if g*eta_o^2 > 0.1
+        	eta = maximum([eta;g*eta_o^2]);
+    	end
+    	eta = maximum([minimum([eta;etamax]);0.5*tol/r_norm]);
+    	#@sprintf("%3d %6d %.3e %.3e %.3e \n", i,k,r_norm,minimum(y),minimum(x));
+        display(r_norm);
+        #res=[res; r_norm];
+	end
+	#@printf("Matrix-vector products = %6d\n", MVP);
+	x=squeeze(x,2);
+	A_balance=diagm(x)*A*diagm(x);
+	return x,A_balance;
+
+end
+
 
 
 
